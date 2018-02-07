0adf50493cd24d749db399cf4291c011f1da14d5
Wei <wei.leung1995@gmail.com>
2018-02-06 20:18:24 -0800

    Committing

--- WindowsApp1/Form1.vb
+++ WindowsApp1/Form1.vb
@@ -12,6 +12,20 @@
     End Sub
 
     Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles usaPictureBox.Click
+        greetingLabel.Text = "Good Day!"
+        exitButton.Text = "Exit"
+    End Sub
 
+    Private Sub Form1_Click(sender As Object, e As EventArgs) Handles Me.Click
+
+    End Sub
+
+    Private Sub belarusPictureBox_Click(sender As Object, e As EventArgs) Handles belarusPictureBox.Click
+        greetingLabel.Text = "Добры дзень"
+        exitButton.Text = "выхад"
+    End Sub
+
+    Private Sub exitButton_Click(sender As Object, e As EventArgs) Handles exitButton.Click
+        End
     End Sub
 End Class
